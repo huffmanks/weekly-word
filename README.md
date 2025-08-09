3ae6ca997b0b7b7097330e871ef406722b410f04
Kevin Huffman <huffmanks2@gmail.com>
2025-08-09 13:08:02 -0400

    update audioPlayer script and astro config.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Weekly Word
 
-https://github.com/trevortylerlee/astro-micro
+## Todo
+
+1. Update pagefind to ignore certain frontmatter. (podcast, etc.)
+2. Make pagefind whole item clickable.
+3. Make fetching RSS feed automated.
--- astro.config.mjs
+++ astro.config.mjs
@@ -6,12 +6,12 @@ import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
+const isDev = process.env.NODE_ENV === "development";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "http://localhost:4321",
-  build: {
-    format: "file",
-  },
+  site: isDev ? "http://localhost:4321" : "https://word.huffmanks.com",
+  base: "/",
   vite: {
     plugins: [tailwindcss()],
   },
