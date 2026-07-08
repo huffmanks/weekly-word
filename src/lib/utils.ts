9bb0089a64a8429687231e698bee6536d591adf7
Kevin Huffman <huffmanks2@gmail.com>
2026-07-08 19:54:41 -0400

    fixed prev/next daily reading and data

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,7 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export function cn(...inputs: ClassValue[]) {
+export function cn(...inputs: Array<ClassValue>) {
   return twMerge(clsx(inputs));
 }
 
@@ -10,4 +10,25 @@ export function readingTime(html: string) {
   const wordCount = textOnly.split(/\s+/).length;
   const readingTimeMinutes = (wordCount / 200 + 1).toFixed();
   return `${readingTimeMinutes} min read`;
+}
+
+export function paramOverrideDateTime(paramDate: string, paramTime: string): void {
+  const targetOverrideString = `${paramDate}T${paramTime}:00`;
+  const overrideCurrentDate = new Date(targetOverrideString);
+
+  const OriginalDate = window.Date;
+
+  function MockedDate(this: Date, ...args: Array<unknown>) {
+    if (args.length === 0) {
+      return overrideCurrentDate;
+    }
+    return new (OriginalDate as unknown as new (...args: Array<unknown>) => Date)(...args);
+  }
+
+  MockedDate.prototype = OriginalDate.prototype;
+  MockedDate.now = () => overrideCurrentDate.getTime();
+  MockedDate.UTC = OriginalDate.UTC;
+  MockedDate.parse = OriginalDate.parse;
+
+  window.Date = MockedDate as unknown as typeof Date;
 }
--- todo.md
+++ todo.md
@@ -1,1 +1,5 @@
 - [ ] Optional songs to sermon that were sung in service
+- [x] Make daily reading prev next buttons floating sticky or fixed.
+- [x] Daily reading and weekly memory make landing page just redirect to correct id page
+- [ ] Check daily reading html version vs text if better
+- [x] Daily reading id page and landing is off by one day
